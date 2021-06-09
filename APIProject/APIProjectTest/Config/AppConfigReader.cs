e221058ff49c7ec8aace61001e01c7f24e5d175b
Michael Willcock <michaelwillcock7@gmail.com>
2021-06-09 11:58:07 +0100

    Base API config set up completed

--- APIProject/APIProjectTest/Config/AppConfigReader.cs
+++ APIProject/APIProjectTest/Config/AppConfigReader.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 
 namespace APIProjectTest
 {
-    class AppConfigReader
+    public static class AppConfigReader
     {
+        public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
+        public static readonly string APIKey = ConfigurationManager.AppSettings["api_key"];
     }
 }
--- APIProject/APIProjectTest/MusicService/DataHandling/DTO.cs
+++ APIProject/APIProjectTest/MusicService/DataHandling/DTO.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace APIProjectTest
 {
-    class DTO
+    public class DTO<ResponseType> where ResponseType : IResponse, new()
     {
+        //
+        public ResponseType Response { get; set; }
+        public void DeserializeResponse(string TrackSearchResponse)
+        {
+            Response = JsonConvert.DeserializeObject<ResponseType>(TrackSearchResponse);
+        }
     }
 }
