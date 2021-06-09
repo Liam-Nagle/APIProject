772a08f490150839c167a640e010e98d2a6d4c38
Michael Willcock <michaelwillcock7@gmail.com>
2021-06-09 14:20:16 +0100

    Created call manager and first service class for MakeTrackRequestByArtistName method :)

---
+++ APIProject/APIProjectTest/MusicService/TrackSearchByArtistNameService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIProjectTest
+{
+    class TrackSearchByArtistNameService
+    {
+        public CallManager CallManager { get; set; }
+        public JObject Json_Response { get; set; }
+        public DTO<TrackResponse> SingleTrackDTO { get; set; }
+        public string TrackResponse { get; set; }
+
+        public TrackSearchByArtistNameService()
+        {
+            CallManager = new CallManager();
+            SingleTrackDTO = new DTO<TrackResponse>();
+        }
+        public async Task MakeHighestRatedTrackByArtistRequestAsync(string artistName, int numSongs)
+        {
+            TrackResponse = await CallManager.MakeHighestRatedTrackByArtistRequestAsync(artistName, numSongs);
+
+            Json_Response = JObject.Parse(TrackResponse);
+
+            SingleTrackDTO.DeserializeResponse(TrackResponse);
+        }
+    }
+}
---
+++ APIProject/APIProjectTest/Tests/TrackSearchByArtistNameServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace APIProjectTest
+{
+    public class TrackSearchServiceTests
+    {
+        TrackSearchByArtistNameService _trackSearchService;
+
+        [OneTimeSetUp]
+        public async Task SetUpAsync()
+        {
+            _trackSearchService = new TrackSearchByArtistNameService();
+            await _trackSearchService.MakeHighestRatedTrackByArtistRequestAsync("Justin Bieber", 3);
+        }
+
+        [Test]
+        public void StatusIs200()
+        {
+            Assert.That(_trackSearchService.CallManager.StatusDescription, Is.EqualTo("OK"));
+        }
+    }
+}
